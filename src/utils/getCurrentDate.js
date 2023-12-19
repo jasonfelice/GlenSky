2410ce79daf8de6dc2d748821737bdce3f79e4f5
Jake Felice <jasonfelice0@gmail.com>
2023-12-19 03:09:41 -0800

    Fix months keys numbering

--- src/utils/getCurrentDate.js
+++ src/utils/getCurrentDate.js
@@ -1,19 +1,19 @@
 const getCurrentDate = (date) => {
   const months = {
-    1: 'January',
-    2: 'February',
-    3: 'March',
-    4: 'April',
-    5: 'May',
-    6: 'June',
-    7: 'July',
-    8: 'August',
-    9: 'September',
-    10: 'October',
-    11: 'November',
-    12: 'December',
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December',
   };
-  
+
   const day = date.toString().slice(0, 3);
 
   return `${day}, ${months[date.getMonth()]} ${date.getDate()}`;

e1e31a2f9650a0493f96ce8acc8a3f224961b8c6
Jake Felice <jasonfelice0@gmail.com>
2023-12-19 02:51:54 -0800

    Add time vector in Splash

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,6 +1,23 @@
-const Splash = () => {
+import Styles from './Splash.module.css';
+import morning from '../../assets/vectors/morning.svg';
+import afternoon from '../../assets/vectors/afternoon.svg';
+import night from '../../assets/vectors/night.svg';
+import getTimeOfDay from '../../utils/getTimeOfDay';
+
+const Splash = ({ date }) => {
+  const timeObject = {
+    morning: [morning],
+    afternoon: [afternoon],
+    night: [night],
+  };
+
   return (
-    <div className="splash"></div>
+    <div className={Styles.splash}>
+      <div className={Styles.vectorWrapper}>
+        <img className={Styles.splashVector} src={timeObject[getTimeOfDay(date)]} alt={getTimeOfDay(date)} />
+      </div>
+      <div className={Styles.citySelect}></div>
+    </div>
   )
 };
 

37da65cdb8776752d6d0e78ce8b858773494f278
Jake Felice <jasonfelice0@gmail.com>
2023-12-19 02:27:11 -0800

    Mock router in App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import {render, screen} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import getCurrentDate from './utils/getCurrentDate';
 import App from './App';
 
 test('if the date is rendered', () => {
-  render(<App />);
-  const date = screen.queryByText(getCurrentDate());
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+    );
+  const date = screen.queryByText(getCurrentDate(new Date()));
   expect(date).toBeVisible();
 });

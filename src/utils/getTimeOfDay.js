a9f06ced20177074d3c88496a4d5f6d3d92668f3
Jake Felice <jasonfelice0@gmail.com>
2023-12-20 08:35:06 -0800

    Fix typo in getTimeOfDay

--- src/utils/getTimeOfDay.js
+++ src/utils/getTimeOfDay.js
@@ -1,7 +1,7 @@
 const getTimeOfDay = (date) => {
   const hour = date.getHours();
   if (hour >= 6 && hour < 12) {
-    return "monring";
+    return "morning";
   } else if (hour >= 12 && hour < 18) {
     return "afternoon";
   } else {

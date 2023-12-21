2066b71f1e6e94f896fc43f39a6159200f588704
Jake Felice <jasonfelice0@gmail.com>
2023-12-21 02:58:53 -0800

    Create cities list component

---
+++ src/components/splash/Cities.js
@@ -0,0 +1,18 @@
+import Styles from './Cities.module.css';
+
+const Cities = () => {
+  return (
+    <ul className={Styles.cities}>
+        <li>Las Vegas, Nevada, USA</li>
+        <li>Las Vegas, Nevada, USA</li>
+        <li>Las Vegas, Nevada, USA</li>
+        <li>Las Vegas, Nevada, USA</li>
+        <li>Las Vegas, Nevada, USA</li>
+        <li>Las Vegas, Nevada, USA</li>
+        <li>Las Vegas, Nevada, USA</li>
+        <li>Las Vegas, Nevada, USA</li>
+    </ul>
+  );
+};
+
+export default Cities;

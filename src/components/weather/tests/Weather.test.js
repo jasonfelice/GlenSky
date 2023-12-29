27cda3b108fed65f36e649a10766dc4f370c4979
Jake Felice <jasonfelice0@gmail.com>
2023-12-29 00:30:21 -0800

    Create all chart tabs test cases

--- src/components/weather/tests/Weather.test.js
+++ src/components/weather/tests/Weather.test.js
@@ -14,4 +14,25 @@ describe('Weather', () => {
     const preChart = screen.queryByLabelText('pre-chart');
     expect(preChart).toBeVisible();
   });
+  test('if the tabs can change to humidity chart', () => {
+    render(<Weather />);
+    const humidTab = screen.queryByText('Humidity');
+    fireEvent.click(humidTab);
+    const humidChart = screen.queryByLabelText('humid-chart');
+    expect(humidChart).toBeVisible();
+  });
+  test('if the tabs can change to wind chart', () => {
+    render(<Weather />);
+    const windTab = screen.queryByText('Wind');
+    fireEvent.click(windTab);
+    const windChart = screen.queryByLabelText('wind-chart');
+    expect(windChart).toBeVisible();
+  });
+  test('if the tabs can change back to temprature chart', () => {
+    render(<Weather />);
+    const tempTab = screen.queryByText('Temperature');
+    fireEvent.click(tempTab);
+    const tempChart = screen.queryByLabelText('temp-chart');
+    expect(tempChart).toBeVisible();
+  });
 });

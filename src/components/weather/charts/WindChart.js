2026ba0256f72894682929420c0b2d8260334290
Jake Felice <jasonfelice0@gmail.com>
2023-12-29 02:19:58 -0800

    Build and style wind chart

--- src/components/weather/charts/WindChart.js
+++ src/components/weather/charts/WindChart.js
@@ -1,6 +1,31 @@
+import arrow from '../../../assets/icons/arrow.svg';
+
 const WindChart = () => {
+  const windWrapperStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 'inherit',
+  };
+
+  const windStyle = {
+    background: `url(${arrow}) no-repeat center center/cover`,
+    width: '50px',
+    height: '50px',
+    display: 'block',
+    filter: 'invert(.3)',
+  };
+
   return (
-    <div aria-label="wind-chart">^</div>
+    <div style={windWrapperStyle} aria-label="wind-chart">
+        <i style={windStyle} />
+        <i style={windStyle} />
+        <i style={windStyle} />
+        <i style={windStyle} />
+        <i style={windStyle} />
+        <i style={windStyle} />
+        <i style={windStyle} />
+    </div>
   )
 };
 

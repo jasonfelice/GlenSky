5c8b725fed3326dad0a7e786f278d6fa235f970b
Jake Felice <jasonfelice0@gmail.com>
2023-12-29 00:21:00 -0800

    Write test case for precipitation chart

--- src/components/weather/tests/Weather.test.js
+++ src/components/weather/tests/Weather.test.js
@@ -2,8 +2,16 @@ import {fireEvent, render, screen} from '@testing-library/react';
 import Weather from '../Weather';
 
 describe('Weather', () => {
-  test('if the user can change temprature state', () => {
+  test('if the temprature chart is displayed by default', () => {
     render(<Weather />);
-    const cel = screen.queryByText('C');
+    const tempChart = screen.queryByLabelText('temp-chart');
+    expect(tempChart).toBeVisible();
+  });
+  test('if the tabs can change to precipitation chart', () => {
+    render(<Weather />);
+    const preTab = screen.queryByText('Precipitation');
+    fireEvent.click(preTab);
+    const preChart = screen.queryByLabelText('pre-chart');
+    expect(preChart).toBeVisible();
   });
 });

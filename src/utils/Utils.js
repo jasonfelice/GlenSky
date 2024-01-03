4cea56158528007805019ff35c565abfecd10761
Jake Felice <jasonfelice0@gmail.com>
2024-01-03 09:06:28 -0800

    Create an Utility class

---
+++ src/utils/Utils.js
@@ -0,0 +1,31 @@
+export default class Utils {
+  getCurrentDate = (date) => {
+    const months = {
+      0: 'January',
+      1: 'February',
+      2: 'March',
+      3: 'April',
+      4: 'May',
+      5: 'June',
+      6: 'July',
+      7: 'August',
+      8: 'September',
+      9: 'October',
+      10: 'November',
+      11: 'December',
+    };
+    const day = date.toString().slice(0, 3);
+    return `${day}, ${months[date.getMonth()]} ${date.getDate()}`;
+  };
+
+  getTimeOfDay = (date) => {
+    const hour = date.getHours();
+    if (hour >= 6 && hour < 12) {
+      return "morning";
+    } else if (hour >= 12 && hour < 18) {
+      return "afternoon";
+    } else {
+      return "night";
+    }
+  };
+};

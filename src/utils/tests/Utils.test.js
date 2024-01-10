51ac8bf261c2a898be9bb0eae3692a4a0d5ad416
Jake Felice <jasonfelice0@gmail.com>
2024-01-10 02:38:55 -0800

    Write test cases for 12 hour time format function

--- src/utils/tests/Utils.test.js
+++ src/utils/tests/Utils.test.js
@@ -15,4 +15,16 @@ describe('Utils Test', () => {
     const result = Utils.getDay('2024-01-05');
     expect(result).toBe('Fri');
   });
+  test('if the correct 12 format is returned. 9am', () => {
+    const result = Utils.getHour('2023-12-29 15:00:00');
+    expect(result).toBe('3pm')
+  });
+  test('Get hour edge case 1. 12am', () => {
+    const result = Utils.getHour('2023-12-29 00:00:00');
+    expect(result).toBe('12am');
+  });
+  test('Get hour edge case 2. 12pm', () => {
+    const result = Utils.getHour('2023-12-29 12:00:00');
+    expect(result).toBe('12pm');
+  });
 });

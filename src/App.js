462927013e1a53fd0697ad1d3edb74f466333493
Jake Felice <jasonfelice0@gmail.com>
2024-01-15 04:11:09 -0800

    Group data into days

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
-import getCurrentDate from './utils/getCurrentDate';
+import Utils from './utils/Utils';
 import './App.css';
 import Weather from './components/weather/Weather';
 import Splash from './components/splash/Splash';
+import data from './tempData'
 
 function App() {
   const date = new Date();
+  const groupedData = {};
+  data.list.forEach(item => {
+  const day = item.dt_txt.split(' ')[0];
+  if (!groupedData[day]) {
+    groupedData[day] = [];
+  }
+  groupedData[day].push(item);
+});
   return (
     <div className="App">
       <div className="rectangle">
-        <span className='date'>{getCurrentDate(date)}</span>
+        <span className='date'>{Utils.getCurrentDate(date)}</span>
         <main>
           <Routes>
             <Route path='/' element={<Splash date={date} />} />
-            <Route path='/weather/:city' element={<Weather />} />
+            <Route path='/weather/:city' element={<Weather data={groupedData} />} />
           </Routes>
         </main>
       </div>

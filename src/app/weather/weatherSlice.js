9a1691be2e705e04ea51890eb8c7ddf202c6efd3
Jake Felice <jasonfelice0@gmail.com>
2024-01-27 00:19:13 -0800

    Create getWeather thunk

--- src/app/weather/weatherSlice.js
+++ src/app/weather/weatherSlice.js
@@ -1,8 +1,22 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const URL = 'https://jsonplaceholder.typicode.com/posts';
+
 const initialState = {
   weather: {},
   status: 'idle',
   error: null,
 };
+
+export const getWeather = createAsyncThunk(
+  'weather/getWeather',
+  async () => {
+    try {
+      const response = await axios.get(URL);
+      return response.data;
+    } catch (error) {
+      return error.message;
+    }
+  },
+);

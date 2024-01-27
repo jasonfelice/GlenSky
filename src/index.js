c1ce48822128691964edb93233c6fd9ff93f2964
Jake Felice <jasonfelice0@gmail.com>
2024-01-27 02:18:29 -0800

    Install react redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './app/store';

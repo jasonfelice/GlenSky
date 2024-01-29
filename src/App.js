26c37bf0003fb7ee618091d58a7656e8c0a3e2af
Jake Felice <jasonfelice0@gmail.com>
2024-01-29 04:42:31 -0800

    Remove temp data from app

--- src/App.js
+++ src/App.js
@@ -3,18 +3,9 @@ import Utils from './utils/Utils';
 import './App.css';
 import Weather from './components/weather/Weather';
 import Splash from './components/splash/Splash';
-import data from './tempData'
 
 function App() {
   const date = new Date();
-  const groupedData = {};
-  data.list.forEach(item => {
-  const day = item.dt_txt.split(' ')[0];
-  if (!groupedData[day]) {
-    groupedData[day] = [];
-  }
-  groupedData[day].push(item);
-});
   return (
     <div className="App">
       <div className="rectangle">
@@ -22,7 +13,7 @@ function App() {
         <main>
           <Routes>
             <Route path='/' element={<Splash date={date} />} />
-            <Route path='/weather/:city' element={<Weather data={groupedData} />} />
+            <Route path='/weather/:city' element={<Weather />} />
           </Routes>
         </main>
       </div>

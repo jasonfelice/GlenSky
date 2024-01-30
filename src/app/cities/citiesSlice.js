25458aa584263e24fd9fab891fd9d53ff72e291f
Jake Felice <jasonfelice0@gmail.com>
2024-01-30 04:22:44 -0800

    Setup cities Slice

---
+++ src/app/cities/citiesSlice.js
@@ -0,0 +1,12 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const G_KEY = process.env.REACT_APP_G_KEY;
+
+
+const initialState = [];
+
+const citiesSlice = {};
+
+export default citiesSlice.reducer;
+

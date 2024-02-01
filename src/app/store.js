02db2b661dab1149a748b1a68f090a46e43cd053
Jake Felice <jasonfelice0@gmail.com>
2024-02-01 06:07:00 -0800

    Import cities slice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import weatherSlice from './weather/weatherSlice';
+import citiesSlice from './cities/citiesSlice';
 
 const store = configureStore({
   reducer: {
     weather: weatherSlice,
+    cities: citiesSlice,
   }
 });
 

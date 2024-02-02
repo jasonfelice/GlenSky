11fb5faece63d66828d6336c28cd2125ec2f1e59
Jake Felice <jasonfelice0@gmail.com>
2024-02-02 03:18:44 -0800

    Import getCities in Splash

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,17 +1,22 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Styles from './Splash.module.css';
 import morning from '../../assets/vectors/morning.svg';
 import afternoon from '../../assets/vectors/afternoon.svg';
 import night from '../../assets/vectors/night.svg';
 import Utils from '../../utils/Utils';
 import Cities from './Cities';
+import { getCities } from '../../app/cities/citiesSlice';
 
 const Splash = ({ date }) => {
+
+  const dispatch = useDispatch();
+  const cities = useSelector((state) => state.cities);
+  const loading = (cities.status === 'pending');
 
   // Check focus state of input elements 
   const [ inputFocus, setInputFocus] = useState(false);
   const [input, setInput] = useState('');
-  const [citiesList, setCitiesList] = useState({ loading: false, cities: []});
 
   // Object for vector images 
   const timeObject = {
@@ -27,11 +32,11 @@ const Splash = ({ date }) => {
       </div>
       <div className={Styles.citySelect}>
         <form aria-label="form"  onClick={() => setInputFocus(true)} className={Styles.inputWrapper}>
-          { citiesList.loading ? (<i className={`${Styles.icon} ${Styles.spin}`} />) : (<i className={`${Styles.icon} ${Styles.enter}`} />)}
+          { loading ? (<i className={`${Styles.icon} ${Styles.spin}`} />) : (<i className={`${Styles.icon} ${Styles.enter}`} />)}
           { !inputFocus && (<p>Las Vegas, Nevada, USA</p>)}
           { inputFocus && (<input autoFocus placeholder='Las Vegas, Nevada, USA' onChange={(e) => setInput(e.target.value)} onBlur={() => setInputFocus(!!input)} type="text" value={input} />)}
-          <i className={`${Styles.icon} ${Styles.find}`} />
-         { (!citiesList.loading && !!citiesList.cities.length) && <Cities cities={citiesList.cities} />}
+          <i onClick={() => dispatch(getCities(input))} className={`${Styles.icon} ${Styles.find}`} />
+         {(!loading && !!cities.list.length) && <Cities cities={cities.list} />}
         </form>
         <i className={Styles.target} />
       </div>

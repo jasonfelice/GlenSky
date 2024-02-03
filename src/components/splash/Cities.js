289c546e90a79bff31d5405c14c1934eec911c32
Jake Felice <jasonfelice0@gmail.com>
2024-02-03 03:45:03 -0800

    Create getCoords function to coordinates of a city

--- src/components/splash/Cities.js
+++ src/components/splash/Cities.js
@@ -1,11 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import Styles from './Cities.module.css';
 
 const Cities = ({ cities }) => {
+  const navigate = useNavigate();
+  const G_KEY = process.env.REACT_APP_G_KEY;
+
+  const getCords = async (id) => {
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${id}&key=${G_KEY}`);
+    return `${response.data.result.geometry.location.lat}-${response.data.result.geometry.location.lng}`;
+  };
   return (
     <ul className={Styles.cities}>
         {
           (Array.isArray(cities)) && cities.map((city) => (
-            <li key={city.place_id}>{city.description}</li>
+            <li
+              // Get weather with coordinates
+              onClick={() => getCords(city.place_id).then((cords) => navigate(`/weather/cords/${cords}`))}
+              key={city.place_id}
+           >{city.description}</li>
           ))
         }
     </ul>

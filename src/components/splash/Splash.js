c2a645a9dc8ca6ea0b7341449861dab6a8df7b53
Jake Felice <jasonfelice0@gmail.com>
2024-02-03 02:18:56 -0800

    Hide cities list when clicked outside of the form

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Styles from './Splash.module.css';
 import morning from '../../assets/vectors/morning.svg';
@@ -11,8 +11,14 @@ import { getCities } from '../../app/cities/citiesSlice';
 const Splash = ({ date }) => {
 
   const dispatch = useDispatch();
+  const wrapperRef = useRef(null);
+
+  // Get cities data from redux store
   const cities = useSelector((state) => state.cities);
   const loading = (cities.status === 'pending');
+
+  // toggle cities list
+  const [toggleCities, setToggleCities] = useState(false);
 
   // Check focus state of input elements 
   const [ inputFocus, setInputFocus] = useState(false);
@@ -25,18 +31,39 @@ const Splash = ({ date }) => {
     night: [night],
   };
 
+  // handle click outside form
+  const handleFormBlur = (e) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(e.target)) {
+      // Clicked outside the component
+      setToggleCities(false);
+    }
+  };
+
+  useEffect(() => {
+    // Attach the event listener on mount
+    document.addEventListener('mousedown', handleFormBlur);
+
+    // set toggle cities as per state
+    setToggleCities((!loading && !!cities.list.length));
+
+    // Detach the event listener on unmount
+    return () => {
+      document.removeEventListener('mousedown', handleFormBlur);
+    };
+  }, [cities, loading]);
+
   return (
     <div className={Styles.splash}>
       <div className={Styles.vectorWrapper}>
         <img draggable={false} className={Styles.splashVector} src={timeObject[Utils.getTimeOfDay(date)]} alt={Utils.getTimeOfDay(date)} />
       </div>
       <div className={Styles.citySelect}>
-        <form aria-label="form"  onClick={() => setInputFocus(true)} className={Styles.inputWrapper}>
+        <form ref={wrapperRef} aria-label="form"  onClick={() => setInputFocus(true)} className={Styles.inputWrapper}>
           { loading ? (<i className={`${Styles.icon} ${Styles.spin}`} />) : (<i className={`${Styles.icon} ${Styles.enter}`} />)}
           { !inputFocus && (<p>Las Vegas, Nevada, USA</p>)}
           { inputFocus && (<input autoFocus placeholder='Las Vegas, Nevada, USA' onChange={(e) => setInput(e.target.value)} onBlur={() => setInputFocus(!!input)} type="text" value={input} />)}
           <i onClick={() => dispatch(getCities(input))} className={`${Styles.icon} ${Styles.find}`} />
-         {(!loading && !!cities.list.length) && <Cities cities={cities.list} />}
+         {toggleCities && <Cities cities={cities.list} />}
         </form>
         <i className={Styles.target} />
       </div>

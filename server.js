95d8856ee7e222d6ca6c0d4649fa6805b4779306
Jake Felice <jasonfelice0@gmail.com>
2024-02-14 02:15:40 -0800

    Install helmet

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'none'"],
+      connectSrc: ["'self'", "localhost:3000"],
+    },
+  })
+);
+
 const PORT = process.env.REACT_APP_OPEN_KEY || 3000;
 
 app.get('/api/places', async (req, res) => {
@@ -13,7 +24,20 @@ app.get('/api/places', async (req, res) => {
       res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
+        throw new Error(error);
     }
+});
+
+app.get('/api/details', async (req, res) => {
+  try {
+    const response = await axios.get('https://maps.googleapis.com/maps/api/place/details/json', {
+      params: req.query,
+    });
+    res.json(response.data);
+  } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+      throw new Error(error);
+  }
 });
 
 app.listen(PORT, () => {

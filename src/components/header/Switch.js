ee2c375f619094a28107bc10d39b327332e5f5d8
Jake Felice <jasonfelice0@gmail.com>
2024-03-28 02:44:35 -0700

    Add light and dark icons to the header switch

--- src/components/header/Switch.js
+++ src/components/header/Switch.js
@@ -2,12 +2,13 @@ import Style from './Switch.module.css';
 
 const Switch = () => {
   return (
-  <>
+  <div style={{display: 'flex', alignItems: 'center'}}>
+    < i class={`${Style.icon} ${Style.sun}`} />
     <label className={Style.switch}>
       <input type="checkbox" />
       <span class={`${Style.round} ${Style.slider}`}></span>
     </label>
-  </>
+  </div>
   );
 };
 
